fe674ada5aa633aafa10b7fa14975b99821cd15e
jflores1 <jflores1@alumni.nd.edu>
2012-11-13 13:56:36 -0500

    Finished leadgen form

--- spec/requests/leads_spec.rb
+++ spec/requests/leads_spec.rb
@@ -21,12 +21,12 @@ describe "Leads" do
 
     describe "a working form" do
       before {visit request_trial_path}
-      let(:submit){ "Try it Out!"}
+      let(:submit){ "Request Access!"}
 
       describe "with valid information" do
         before do
           fill_in "name",               with: "Jesse Flores"
-          choose  "teacher"
+          select  "Teacher"
           fill_in "school",             with: "Holy Spirit Prep"
           fill_in "email",              with: "jesse@test.com"
           fill_in "phone",              with: "555-555-5555"
