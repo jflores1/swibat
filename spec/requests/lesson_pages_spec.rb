2a65d61e2692789e2344e7c8355b58607ec22f63
jflores1 <jflores1@alumni.nd.edu>
2012-11-27 17:17:10 -0500

    refactor lesson pages spec

--- spec/requests/lesson_pages_spec.rb
+++ spec/requests/lesson_pages_spec.rb
@@ -12,33 +12,54 @@ describe "LessonPages" do
       it {response.status.should be(200)}
     end
 
-    describe "Add a lesson" do
+    describe "Submits a form to add a lesson" do
       let(:submit){"Submit"}
       let(:add_another_lesson){"Add Another Lesson"}
       let(:lesson){unit.lessons}
 
-      context "With valid information" do
+      context "With valid attributes" do
         before {
           sign_in_and_go_to_page
         }
-        it "should add a lesson and then another" do
-          expect {
-            fill_in_form_with_valid_information
-            click_button add_another_lesson
-          }.to change(lesson, :count).by(1)
-          current_path.should == new_unit_lesson_path(unit)
+
+        context "Without a resource" do
+          it "should add a lesson and then another" do
+            expect {
+              fill_in_form_with_valid_information
+              click_button add_another_lesson
+            }.to change(lesson, :count).by(1)
+            current_path.should == new_unit_lesson_path(unit)
+          end
+
+          it "should add a lesson and go back to user page" do
+            expect {
+              fill_in_form_with_valid_information
+              click_button submit
+            }.to change(lesson, :count).by(1)
+            current_path.should == user_path(@user)
+          end
         end
 
-        it "should add a lesson and go back to user page" do
-          expect {
-            fill_in_form_with_valid_information
-            click_button submit
-          }.to change(lesson, :count).by(1)
-          current_path.should == user_path(@user)
+        context "With resources" do
+          it "should add a lesson with resources" do
+            fill_in 'lesson_title',     				with: "My lsson"
+            fill_in 'lesson_start_date',    	 	with: "2011-01-01"
+            fill_in 'lesson_end_date',	    	 	with: "2012-01-01"
+            fill_in 'lesson_status', 					  with: "Started"
+            fill_in 'prior_knowledge', 					with: "None"
+            fill_in 'lesson_resources_attributes_0_name', 	with: "Lesson notes"
+            page.attach_file('lesson_resources_attributes_0_upload', Rails.root + 'spec/fixtures/files/upload.txt')
+            expect { click_button submit }.to change(Lesson, :count).by(1)
+            Lesson.last.resources.count.should == 1
+            resource = Resource.last
+            resource.name.should == "Lesson notes"
+            resource.upload_file_name.should == "upload.txt"
+          end
         end
+
       end
 
-      context "With invalid information" do
+      context "With invalid attributes" do
         before {sign_in_and_go_to_page}
         it "should not add a lesson" do
           expect {
@@ -47,6 +68,11 @@ describe "LessonPages" do
           }.to_not change(lesson, :count).by(1)
         end
         it {current_path.should == new_unit_lesson_path(unit)}
+
+        it "should show errors on invalid submit" do
+          click_button submit
+          page.should have_selector('#error_explanation')
+        end
       end
     end
 
@@ -57,58 +83,6 @@ describe "LessonPages" do
       get new_unit_lesson_path(unit)
       response.status.should be(302)
     end
-  end
-
-  context "With Resources" do
-      before do
-          sign_in_via_form
-          @course = FactoryGirl.create(:course)
-          @unit = FactoryGirl.create(:unit, :course => @course)
-      end
-      
-      describe "Creating a new lesson" do
-          before { visit new_unit_lesson_path @unit }
-          let(:submit_button){"Submit"}
-          
-          it "should display the form" do
-              page.should have_selector('form')
-          end
-          
-          describe "With invalid attributes" do
-              it "should show errors on invalid submit" do
-                  click_button submit_button
-                  page.should have_selector('#error_explanation')
-              end
-          end
-          
-          describe "With valid attributes and no resources" do
-              it "should submit the form and create the lesson" do
-                  fill_in 'lesson_title',     				with: "My lsson"
-                  fill_in 'lesson_start_date',    	 	with: "2011-01-01"
-                  fill_in 'lesson_end_date',	    	 	with: "2012-01-01"
-                  fill_in 'lesson_status', 					  with: "Started"
-                  fill_in 'prior_knowledge', 					with: "None"
-                  expect { click_button submit_button }.to change(Lesson, :count).by(1)
-              end
-          end
-          
-          describe "With valid attributes and resources" do
-              it "should submit the form and create the lesson with resources" do
-                  fill_in 'lesson_title',     				with: "My lsson"
-                  fill_in 'lesson_start_date',    	 	with: "2011-01-01"
-                  fill_in 'lesson_end_date',	    	 	with: "2012-01-01"
-                  fill_in 'lesson_status', 					  with: "Started"
-                  fill_in 'prior_knowledge', 					with: "None"
-                  fill_in 'lesson_resources_attributes_0_name', 	with: "Lesson notes"
-                  page.attach_file('lesson_resources_attributes_0_upload', Rails.root + 'spec/fixtures/files/upload.txt') 
-                  expect { click_button submit_button }.to change(Lesson, :count).by(1)
-                  Lesson.last.resources.count.should == 1
-                  resource = Resource.last
-                  resource.name.should == "Lesson notes"
-                  resource.upload_file_name.should == "upload.txt"
-              end
-          end
-      end
   end
     
   private
