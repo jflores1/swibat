f9f55087d8fbf6ce90404f81682b3bb8e62f761c
jflores1 <jflores1@alumni.nd.edu>
2013-01-03 16:24:42 -0500

    Fixes typo in pdfkit configuration file

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -5,7 +5,7 @@ PDFKit.configure do |config|
      :margin_top    => '0.5in',
      :margin_right  => '1.0in',
      :margin_bottom => '0.5in',
-     :margin_left   => '1.0in'
+     :margin_left   => '1.0in',
      :print_media_type => true
    }
   # config.root_url = "http://localhost" # Use only if your external hostname is unavailable on the server.
