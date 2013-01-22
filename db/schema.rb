acc5e09d2689d25bde6f1ef22a4dbc45969f8f1d
jflores1 <jflores1@alumni.nd.edu>
2013-01-22 11:06:22 -0500

    Adds omniauth_khan_academy support but validations fail.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122050601) do
+ActiveRecord::Schema.define(:version => 20130122153550) do
 
   create_table "activities", :force => true do |t|
     t.string   "activity"
@@ -357,8 +357,8 @@ ActiveRecord::Schema.define(:version => 20130122050601) do
   add_index "units", ["course_id"], :name => "index_units_on_course_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",        :null => false
+    t.string   "encrypted_password",     :default => "",        :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -367,9 +367,9 @@ ActiveRecord::Schema.define(:version => 20130122050601) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "role"
+    t.datetime "created_at",                                    :null => false
+    t.datetime "updated_at",                                    :null => false
+    t.string   "role",                   :default => "teacher"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "institution"
@@ -378,6 +378,8 @@ ActiveRecord::Schema.define(:version => 20130122050601) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.text     "profile_summary"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
