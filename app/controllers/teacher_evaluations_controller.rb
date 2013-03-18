bc84107fce37fa71528e4501bc2bbbd03be1132a
Marjan Georgiev <marjang.georgiev@gmail.com>
2013-03-18 17:34:37 +0100

    Changes the d3 graph. It now displays the score per criterion rather than domain

--- app/controllers/teacher_evaluations_controller.rb
+++ app/controllers/teacher_evaluations_controller.rb
@@ -11,12 +11,37 @@ class TeacherEvaluationsController < ApplicationController
   	@evaluation = TeacherEvaluation.find(params[:id])
   	@user = @evaluation.teacher
 
-
     @data = []
     @evaluation.evaluation_template.evaluation_domains.each do |domain|
-      @data << {name: domain.name , score: domain.calculate_score(@evaluation).to_f}
+      domain_hash = {domain: domain.name}
+      domain.evaluation_criteria.each do |criterion|
+        @data << {name: criterion.contents , score: EvaluationRating.find_by_criterion_id_and_evaluation_id(criterion.id, @evaluation.id).score, domain: domain.name}
+      end
+
+      @data << domain_hash
     end
     @json_data = @data.to_json
+    
+
+    # Data for the alternative view
+    # @data = {}
+    # @domains = []
+    # @criteria = []
+    # @evaluation.evaluation_template.evaluation_domains.each do |domain|
+    #   domain_hash = {domain: domain.name}
+    #   domain_hash[:criteria] = []
+    #   domain.evaluation_criteria.each do |criterion|
+    #     @criteria << {name: criterion.contents}
+    #     domain_hash[:criteria] << {name: criterion.contents , score: EvaluationRating.find_by_criterion_id_and_evaluation_id(criterion.id, @evaluation.id).score}
+    #   end
+
+    #   @domains << domain_hash
+    # end
+    # @data[:domains] = @domains
+    # @data[:criteria] = @criteria
+    # @json_data = @data.to_json
+
+
   end
 
   def create
